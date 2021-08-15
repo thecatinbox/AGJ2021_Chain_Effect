4da360e498313fea5fad7d2ffb7f1de9ea7b2c2f
thecatinbox <catinboxx@gmail.com>
2021-08-14 20:20:48 -0600

    music fix

--- Script/Title.gd
+++ Script/Title.gd
@@ -5,15 +5,15 @@ var music = load("res://Music/Music.ogg")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AudioStreamPlayer.stream = music
-	$AudioStreamPlayer.play(Global.musicPos)
+	$AudioStreamPlayer.play() # Global.musicPos
 	pass 
 
 func _process(delta):
 	OS.window_size = Vector2(1024,600)
-	Global.musicPos = $AudioStreamPlayer.get_playback_position()
 
 func _on_StartButton_pressed():
 	get_tree().change_scene("res://Scene/Scene.tscn")
+	Global.musicPos = $AudioStreamPlayer.get_playback_position()
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
